6dd521d9f1f60df67ae75afa2cbc9f3de2f308ce
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-15 12:31:29 -0600

    finished binary search

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,6 +1,18 @@
 function binarySearch(arr, target){
-    leftIndex = arr.length[0];
-    rightIndex= arr.length -1;
-    middle = leftIndex + rightIndex /2;
+    let leftIndex = [0];
+    let rightIndex= arr.length -1;
     
+
+    while( leftIndex <= rightIndex){
+        let middleIndex = Math.floor(leftIndex + rightIndex) /2;
+        if (target === arr[middleIndex]){
+            return middleIndex
+        }
+        if(target < arr[middleIndex]){
+            rightIndex = middleIndex -1
+        }else {
+            leftIndex = middleIndex +1
+        }
+    }
+    return -1
 }
