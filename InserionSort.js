d93fd6aec75922396628974048ec3bf33281de34
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-16 12:09:13 -0600

    added insertion sort

--- InserionSort.js
+++ InserionSort.js
@@ -1,5 +1,13 @@
-function insertionSort(){
-    
+function insertionSort(arr){
+    for(let i=0; i < arr.length; i++){
+        let numberToInsert = arr[i];
+        let j = i - 1;
+        while( j >= 0 && arr[j] > numberToInsert){
+            arr[j+1] = arr[j]
+            j = j-1;
+        }
+    }
+    arr[j +1] = numberToInsert
 }
 
 
