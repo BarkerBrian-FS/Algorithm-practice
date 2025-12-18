7fcfdc9f8f5d1b9eebc61c47e130137f03af3916
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-18 12:39:09 -0600

    added filter array problem

---
+++ FilterArrayItems.js
@@ -0,0 +1,12 @@
+//
+
+
+let transform = function (arr, fn){
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+    if(fn(arr[i], Number(i))){
+        res.push(arr[i])
+    }
+  }
+  return res
+}
