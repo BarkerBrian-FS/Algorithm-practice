3f8af5594f4706fb05ff18aa023489b430844276
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-19 12:21:58 -0600

    added group anagrams problem

---
+++ groupAnagrams.js
@@ -0,0 +1,26 @@
+//Brute Force
+function groupAnagrams (strs){
+    let cache = {};
+    for(let str of strs){
+        let sortedKey = str.split('').sort().join('');
+        (!cache[sortedKey]) ? cache[sortedKey] = str : cache[sortedKey].push(str)
+    }
+    return Object.values(cache);
+}
+
+//Optimized
+function groupAnagrams(strs) {
+  const map = new Map();
+
+  for (const str of strs) {
+    const count = Array(26).fill(0);
+    for (const char of str) {
+      count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    const key = count.join('#'); // create a string key from frequency array
+    if (!map.has(key)) map.set(key, []);
+    map.get(key).push(str);
+  }
+
+  return Array.from(map.values());
+}
