5da2c2b584f89c7b5faf9c748b7bd43b6dc68ea3
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-20 12:35:02 -0600

    added solution to remove element

---
+++ RemoveElement.js
@@ -0,0 +1,10 @@
+let removeElement = function(nums, val){
+    if(!nums) return 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] == val){
+            nums.splice(i, 0);
+            i--;
+        }
+    }
+    return nums.length
+}
