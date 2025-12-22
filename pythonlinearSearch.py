8099723c8d07960f32858b9811d55ebf3efa2bba
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-22 11:33:16 -0600

    added verify function to python linear search

--- pythonlinearSearch.py
+++ pythonlinearSearch.py
@@ -3,3 +3,10 @@ def linear_search(list, target):
         if list[i] == target:
             return i
     return None
+
+
+def Verify (index):
+    if index is not None:
+        print("Target found at index:", index)
+    else:
+        print("Target not found in list")
