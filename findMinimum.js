b6c37868683ffc765e1f8b527ffb12a650ce289b
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-26 11:24:12 -0600

    added audience followers practice algorithm

--- findMinimum.js
+++ findMinimum.js
@@ -13,3 +13,19 @@ function findMinimum(nums) {
 
   return minimum;
 }
+
+
+//audience followers practice
+
+function get_estimated_spread(audiences_followers){
+    if(audiences_followers.length === 0){
+        return 0
+    }
+    let total_followers = 0
+    for (followers in audiences_followers){
+        total_followers += followers
+    }
+    let average_audience_followers = total_followers / audiences_followers.length
+        let estimated_spread = average_audience_followers * (audiences_followers.length ** 1.2)
+        return estimated_spread
+}
