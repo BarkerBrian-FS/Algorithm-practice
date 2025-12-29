bd98fd944d08c224528e27268daccdbc29e788b5
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-12-29 11:56:36 -0600

    added insertion sort with explanation

--- InserionSort.js
+++ InserionSort.js
@@ -17,3 +17,20 @@ insertionSort(arr);
 console.log(arr);
 
 //Big O = O(n2)
+
+function insertionSort(nums) {
+    //loops through nums array
+    for (let i = 0; i < nums.length; i++) {
+        //adds j pointer at i
+        let j = i;
+        //starts while loop with 2 conditions
+        //j > 0 ensures j doesn't go passed beginning
+        //2nd condition checks if number to left is larger and then leads to a swap
+        while (j > 0 && nums[j - 1] > nums[j]) {
+            [nums[j], nums[j - 1]] = [nums[j - 1], nums[j]];
+            //sends one more to the left
+            j--;
+        }
+    }
+    return nums;
+}
