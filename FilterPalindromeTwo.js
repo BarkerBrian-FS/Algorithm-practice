3444a98cbf51ee16810172c7a98d0d0527713c9c
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-01-09 12:11:06 -0600

    added filter palindrome two pointers algorithm

---
+++ FilterPalindromeTwo.js
@@ -0,0 +1,43 @@
+"use strict";
+
+function isAlphaNumeric(c) {
+    return /^[a-zA-Z0-9]*$/.test(c);
+}
+
+function isPalindrome(s) {
+    const arr = s.split("").filter((c) => isAlphaNumeric(c));
+    let l = 0;
+    let r = arr.length - 1;
+    while (l < r) {
+        if (arr[l].toLowerCase() !== arr[r].toLowerCase()) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+
+function* main() {
+    const s = yield;
+    const res = isPalindrome(s);
+    console.log(res);
+}
+
+class EOFError extends Error {}
+{
+    const gen = main();
+    const next = (line) => gen.next(line).done && process.exit();
+    let buf = "";
+    next();
+    process.stdin.setEncoding("utf8");
+    process.stdin.on("data", (data) => {
+        const lines = (buf + data).split("\n");
+        buf = lines.pop();
+        lines.forEach(next);
+    });
+    process.stdin.on("end", () => {
+        buf && next(buf);
+        gen.throw(new EOFError());
+    });
+}
