30430ef0c69fd58ab7c498709afe2ad03c703ee8
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-01-20 11:42:34 -0600

    added max prod three

--- MaxProdThree.js
+++ MaxProdThree.js
@@ -1,4 +1,18 @@
+/*
+Given an integer array, find the maximum product of any three numbers.
+This problem demonstrates why sorting helps reveal the solution structure. 
+With negative numbers in the array, the maximum product isn't always the three largest numbers.
+*/
+
 function maxProductOfThree(nums) {
-    
-    return 0;
+    //sort array in ascending order
+    nums.sort((a,b)=> a-b);
+
+    let n = nums.length;
+    // caculate the products found in the array 
+    // all three largest number chosen and two largest negative and largest number chosen 
+    let product1 = nums[n-1] * nums[n-2]* nums[n-3];
+    let product2 = nums[0] * nums[1] * nums[n-1];
+    //math max compares and returns largest product
+    return Math.max(product1, product2)
 }
