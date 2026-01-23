681525b48195337fa4e6296bbb45fb9cb9ccf867
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-01-23 11:48:11 -0600

    added find square root

--- firstElementNotSmallerThanTaget.js
+++ firstElementNotSmallerThanTaget.js
@@ -18,3 +18,42 @@ function firstNotSmaller(arr, target){
     }
     return boundaryIndex
 }
+
+//Find first occurence
+
+function firstOccurence(arr, target){
+    let left = 0;
+    let right = arr.length -1;
+    let targetIndex = -1
+    while(left <= right){
+        if (arr[mid] === target){
+            targetIndex = mid
+            right = mid -1
+        } else if( arr[mid] < target){
+            left = mid+1
+        } else {
+            right = mid -1
+        }
+    }
+    return targetIndex
+}
+
+//Find square root
+
+function findSquareRoot(n){
+    if(n===0) return 0;
+    let left = 1;
+    let right = n;
+    let res = -1;
+    while(left <= right){
+        let mid = Math.floor((left + right)/2);
+        if(mid * mid === n){
+            return mid;
+        } else if(mid * mid > n){
+            right = mid -1;
+        }else {
+            left = mid + 1
+        }
+    }
+    return res -1
+}
