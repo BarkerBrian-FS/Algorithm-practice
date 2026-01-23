7427b21322643e3139d24dbfd35dd136b2a908f3
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-01-23 11:05:47 -0600

    added first not smaller function

---
+++ firstElementNotSmallerThanTaget.js
@@ -0,0 +1,20 @@
+/*
+Given an array of integers sorted in increasing order and a target, find the index of the first element in the array that is larger than or equal to the target. Assume that it is guaranteed to find a satisfying number.
+*/
+
+function firstNotSmaller(arr, target){
+    let left = 0;
+    let right = arr.length -1;
+    let boundaryIndex = -1;
+
+    while(left <= right){
+        let mid = Math.floor((left + right)/2)
+        if(arr[mid] >= target){
+            boundaryIndex = mid
+            right = mid -1
+        } else {
+            let = mid + 1
+        }
+    }
+    return boundaryIndex
+}
