e00aa82ab3208590e5282924d2fad25e7f7b9dc4
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-01-27 11:38:07 -0600

    added two pointer move zeros

---
+++ TwoPointerMoveZeros.js
@@ -0,0 +1,15 @@
+/*
+Given an array of integers, move all the 0s to the back of the array while maintaining the relative order of the non-zero elements. 
+Do this in-place using constant auxiliary space.
+*/
+
+
+function moveZeros(nums){
+    let slow = 0;
+    for(let fast=0; fast < nums.length; fast++){
+        if(nums[fast] !== 0){
+            [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
+            slow++
+        }
+    }
+}
