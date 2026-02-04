a1af8936055448848b5c2e907c5085864cd31a7a
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-04 11:40:28 -0600

    added two sum with map

--- containesDuplicates.js
+++ containesDuplicates.js
@@ -1,9 +1,24 @@
+//contains duplicates
 function containsDuplicates(nums){
+    //make a set
     let seen = new Set()
-
+    //loop through nums 
     for(let num of nums){
+        //check if num is in set
         if(seen.has(num)) return true;
+        //add the num otherwise
         seen.add(num)
     }
+    //no duplicates return false
     return false
+}
+
+//twoSum
+function twoSum(nums, target){
+    let map = new Map();
+    for(let i=0; i< nums.length; i++){
+        const complement = target -nums[i];
+        if(map.has(complement)) return [map.get(complement), i]
+        map.set(nums[i], i)
+    }
 }
