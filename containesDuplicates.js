b4db38a411dd8b5b3031c903d88dfb25824e0dc2
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-04 12:52:38 -0600

    added is number palindrome

--- containesDuplicates.js
+++ containesDuplicates.js
@@ -26,4 +26,53 @@ function twoSum(nums, target){
     return [];
 }
 
-//three sum 
+//two sum brute
+function bruteTwoSum(nums, target){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i , j]
+            }
+        }
+    }
+    return [];
+}
+
+// valid parenthesis
+
+function isValid(s){
+    const stack = [];
+    const mapping = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    };
+    for(let char of s){
+        if(char in mapping){
+            if(stack.length ===0 || stack.pop() !== mapping[char]){
+                return false
+            }
+        } else {
+            stack.push(char)
+        }
+    }
+    return stack.length === 0;
+}
+
+
+//is number palindrome
+function isPalindrome(x){
+    // no negatives or any number ending in zero
+    if (x < 0 ||(x !== 0 && x % 10 ===0)){
+        return false
+    }
+    let reversedHalf = 0;
+    //reverse only to the middle
+    while(x > reversedHalf){
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x = Math.floor(x / 10)
+    }
+    //even digits x should equal reversedHalf
+    //Odd digits x shoud equal reversedHalf without middle digit 
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+}
