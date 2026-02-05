6ae4e553c9adf61023c1e101080f686dd7f6ff55
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-05 12:18:44 -0600

    added binary search

--- codingTemplates.js
+++ codingTemplates.js
@@ -199,3 +199,23 @@ function binaryTreeBFS(root){
     }
     return ans;
 }
+
+//Binary Search
+function binarySearch( arr, target){
+    let left = 0;
+    let right = arr.length -1;
+    while(left <= right){
+        let mid = Math.floor((left + right)/2)
+        if(arr[mid] === target){
+            //do something
+            return mid
+        }
+        if(arr[mid] > target){
+            right = mid -1
+        }else {
+            left = mid +1
+        }
+    }
+    //left is the insertion point
+    return left
+}
