fc3d89998d4140120f576bc1dc02123996d6b376
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-06 10:57:54 -0600

    add is num a palidrome

--- FilterPalindromeTwo.js
+++ FilterPalindromeTwo.js
@@ -56,3 +56,17 @@ function isPalindrome(word){
     }
     return true
 }
+
+// Interview preferred mathematical solution number palidrome
+
+function numPalidrome(x){
+    if(x < 0 || (x % 10 === 0 && !== 0)) return false;
+
+    let reversedHalf = 0;
+
+    while(x > reversedHalf){
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+}
