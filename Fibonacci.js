be597dc6c25c324f9d8270f912180d17c330685c
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-13 12:11:25 -0600

    added tribonacci with signature function

--- Fibonacci.js
+++ Fibonacci.js
@@ -30,3 +30,25 @@ function productFib(prod){
     //returns arrays of nums used and boolean 
     return [a, b, a * b === prod]
 }
+
+
+//Tribonacci sequence
+//signature is array of 3 nums to start sequence
+//n is the num of elements to return
+function tribonacci(signature, n){
+    //edge case for n = 0 
+    if(n === 0 ) return [];
+    //edge case for less than 3 in signature
+    if(n <= 3) return signature.slice(0,n);
+    //recursive call to get first n - 1 nums of sequence
+    const prev = tribonacci(signature, n -1);
+    //calculate the next number in the sequence
+    const next = 
+    prev[prev.length - 1] +
+    prev[prev.length - 2] +
+    prev[prev.length - 3];
+    //add new trib numbers to end of array
+    prev.push(next);
+
+    return prev;
+}
