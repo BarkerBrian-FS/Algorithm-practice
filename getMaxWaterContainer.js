3bbe9c5cab8ea5afbb450d26791b2343ff6fbe7a
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-13 11:02:12 -0600

    added optimized get max container function

--- getMaxWaterContainer.js
+++ getMaxWaterContainer.js
@@ -18,3 +18,32 @@ function getMaxWaterContainer(heights){
     //return largest 
     return maxArea 
 }
+
+//get max container optimized
+
+function getMaxContainer(heights){
+    //make first pointer at beginning 
+    let p1 = 0
+    //make second pointer at end
+    let p2 = heights.length - 1
+    //make area variable for largest
+    let maxArea=0
+    //while first pointer less than second
+    while(p1 < p2){
+        //find largest height between both pointers
+        const height = Math.min(heights[p1] - heights[p2]);
+        //subtract indexes to find width
+        const width = p2 - p1;
+        //find area between heights
+        const area = height * width;
+        //check if area larger than max area and switch 
+        maxArea = Math.max(maxArea, area)
+        //if still room available move pointers 
+        if(heights[p1] <= heights[p2]){
+            p1++
+        } else{
+            p2--
+        }
+        return maxArea
+    }
+}
