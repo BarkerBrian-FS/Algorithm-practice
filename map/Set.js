e2cd1047bda46b5fa091169758b8a132c23b5512
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-17 11:46:32 -0600

    added map crash course to algos

--- map/Set.js
+++ map/Set.js
@@ -32,3 +32,44 @@ return longest;
 //Map crash course
 
 //Frequency Counter Pattern
+function countChar(str){
+    const map = new Map();
+
+    for(let char of str){
+        map.set(char, (map.get(char)||0) +1) //this line is a key value pair
+    }
+    return map 
+}
+
+//map patter Two Sum
+function twoSum(nums, target){
+    const map = new Map();
+
+    for (let i=0; i< nums.length; i++){
+        const complement = target - nums[i]
+
+        if(map.has(complement)){
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+}
+
+//Map Pattern Grouping
+function groupByLength(words){
+    const map = new Map();
+
+    for(let word of words){
+        //store word length as key
+        const key = word.length;
+        //check if key exists 
+        if(!map.has(key)){
+            //if no key set key and array for word
+            map.set(key, []);
+        }
+        //get the key and push word into array
+        map.get(key).push(word)
+    }
+    //transform map into array and return all 
+    return Arrray.from(map.values())
+}
