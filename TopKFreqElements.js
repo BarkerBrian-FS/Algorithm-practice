0608b2e6ad656490b5aab02b98e6c15c414769af
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-17 12:35:34 -0600

    added majority element

--- TopKFreqElements.js
+++ TopKFreqElements.js
@@ -21,3 +21,23 @@ function topKFrequent(nums, k){
     }
     return result.slice(0, k);
 }
+
+//Majority Element
+
+function majorityElement(nums){
+    //create map to store number and frequency of it 
+    const counts = new Map();
+    //stores length of the array
+    const n = nums.length;
+    //iterates through nums making freq a 0 or adding one
+    for(const num of nums){
+        counts.set(num, (counts.get(num)||0) +1)
+    }
+    //loops through map counts.entries give array of key value pairs 
+    for(const [num, count]of counts.entries()){
+        //checks if numbers count greater than half array length
+        if(count > n/2){
+            return num 
+        }
+    }
+}
