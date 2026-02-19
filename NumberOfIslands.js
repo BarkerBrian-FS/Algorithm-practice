de9231d8417c5caba46c90c3d500f12161e10e12
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-19 11:44:22 -0600

    added number of islands for dfs practice

--- NumberOfIslands.js
+++ NumberOfIslands.js
@@ -1,23 +1,46 @@
-const numIslands = (grid) => {
-    let countIslands = 0;
+function numIslands(grid){
+    //edge case if grid null or no rows return 0
+    if(!grid || grid.length === 0)return 0;
+    //count for the islands
+    let count = 0;
+    //helper function takes row and column
+    function dfs(r, c){
+        if(
+            //checks if we went above grid
+            r < 0 ||
+            //checks if we went left of grid
+            c < 0 ||
+            //checks if we went below grid
+            r >= grid.length ||
+            //checks if we went right of grid
+            c >= grid[0].length ||
+            //checks if water or visited
+            grid[r][c] === 0
+        ){
+            return;
+        }
+        //prevent double counting by changing 1 to 0
+        grid[r][c] === 0;
 
-    for(let rowIndex in grid){
-        for (let colIndex in grid[rowIndex]){
-            if(grid[rowIndex][colIndex] === '1'){
-                countIslands++;
-                terraform(parseInt (rowIndex), parseInt(colIndex),grid);
+        //explore neighbors using recursion 
+        // check all directions from start
+        dfs(r + 1, c) //down
+        dfs(r - 1, c) //up
+        dfs(r, c + 1) //right
+        dfs(r, c - 1) //left 
+    }
+    //loops through all rows
+    for(let r = 0; r < grid.legth; r++){
+        //loop through all columns
+        for(let c = 0; c < grid.length; c++){
+            //if land found 
+            if(grid[r][c] === 1){
+                //increse island count
+                count++
+                //use dfs to turn 1 to 0
+                dfs(r, c)
             }
         }
     }
-
-    return countIslands
-}
-
-const terraform = (rowIndex, colIndex, grid) => {
-    if(grid[rowIndex][colIndex] === undefined || grid[colIndex]===undefined||grid[rowIndex][colIndex]==='0') return;
-
-    terraform(rowIndex +1, colIndex, grid);
-    terraform(rowIndex -1, colIndex, grid);
-    terraform(rowIndex, colIndex+1, grid);
-    terraform(rowIndex, colIndex-1, grid);
+    return count;
 }
