bd910bec453f51d6bdbc60ef4680d20e0a9359ac
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-19 11:00:33 -0600

    added two pointer merge alternate

--- TwoPoinerOppMostWater.js
+++ TwoPoinerOppMostWater.js
@@ -18,3 +18,34 @@ function containerWithMostWater(height){
     }
     return maxArea
 }
+
+//Merge alternately
+/*
+You are given two strings word1 and word2. 
+Merge the strings by adding letters in alternating order, 
+starting with word1. If a string is longer than the other, 
+append the additional letters onto the end of the merged string.
+Return the merged string.
+*/
+function mergeAlternate(word1, word2){
+    let result = '';
+    let i= 0;
+    let j =0;
+
+    while(i<word1.length && j<word2.length){
+        result+=word1[i];
+        result += word2[j];
+        i++;
+        j++;
+
+    }
+    while(i<word1.length){
+        result+=word1[i];
+        i++;
+    }
+    while(i<word2.length){
+        result+=word1[j];
+        j++;
+    }
+    return result;
+}
