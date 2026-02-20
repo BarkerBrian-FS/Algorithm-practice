c02d29167726f68c0c14fbd67074403c1b4e7bcd
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-20 11:43:56 -0600

    added print method to linked list

--- CreateLinkedList.js
+++ CreateLinkedList.js
@@ -23,7 +23,15 @@ class LinkedList{
         }
         return current
     }
-
+    print(){
+        let output = '';
+        let current = this.head;
+        while(current){
+            output = `${output}${current.value}`;
+            current = current.next;
+        }
+        console.log(`${output}null`)
+    }
 }
 
 class LinkedListNode{
