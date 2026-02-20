4a45dd1afb9e27a6635d2f1ba128f0cf58b2028c
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-20 12:18:49 -0600

    added creat queue with enqueue method

---
+++ CreateQueue.js
@@ -0,0 +1,32 @@
+//Creatind Queue from scratch
+//first in first out added to back removed from front
+
+class QueueElement{
+    constructor(value){
+        this.value = value;
+        this.behind = null;
+    }
+   
+}
+
+class Queue{
+    constructor(){
+        this.front = null;
+        this.back = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        const newQueueElement = new QueueElement(val);
+        if(!this.front){
+            this.front = newQueueElement;
+            this.back = newQueueElement;
+        } else {
+            const prevBack = this.back;
+            prevBack.behind = newQueueElement;
+            this.back = newQueueElement
+        }
+        this.size = this.size+1;
+        return this.size;
+    }
+    
+}
