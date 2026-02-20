28d2860408633542ffcb600e215aab1afef95714
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-20 11:29:11 -0600

    added list and node constructor and insert method

--- CreateLinkedList.js
+++ CreateLinkedList.js
@@ -3,3 +3,23 @@
 //Singly goes forward, Doubly goes both ways
 //Circular Linked List has a cycle
 
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.length = 0;
+    }
+
+    insertAtHead(data){
+        const newNode = new LinkedListNode(data, this.head)
+        this.head = newNode;
+        this.length++
+    }
+
+}
+
+class LinkedListNode{
+    constructor(value, next){
+        this.value = value;
+        this.next = next;
+    }
+}
