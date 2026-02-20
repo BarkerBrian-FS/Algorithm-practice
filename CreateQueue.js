b60bbae5fe10a91cc72e9532a571542a7ca919c8
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-20 12:21:49 -0600

    added dequeue method to queue

--- CreateQueue.js
+++ CreateQueue.js
@@ -28,5 +28,18 @@ class Queue{
         this.size = this.size+1;
         return this.size;
     }
-    
+    dequeue(){
+        if(!this.front){
+            return null;
+        }
+        const prevFront = this.front;
+        if(this.front === this.back){
+            this.front = null;
+            this.back = null;
+        } else {
+            this.front = this.front.behind;
+        }
+        this.size = this.size - 1;
+        return prevFront.value;
+    }
 }
