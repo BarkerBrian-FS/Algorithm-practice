56a65ef96b53626e5587cafb25691d6b430d9619
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-21 11:14:47 -0600

    added solution to valid parenthesis

--- validParethesis.js
+++ validParethesis.js
@@ -1,24 +1,19 @@
 function isValid(s){
     const stack = [];
     const map = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
-    
-    for(let char of s){
-        if(char === '('|| char === "{"|| char === "["){
-            //push opening brackets onto stack 
-            stack.push(char);
-        }else{
-            //if stack is empty or top doesnt match invalid
-            if(stack.length === 0 || stack[stack.length -1] !== map[char]){
+        ')':'(',
+        '}':'{',
+        ']':'['
+    }
+
+    for(char of s){
+        if(char === '('|| char ==='{' || char ==='['){
+            stack.push(char)
+        } else{
+            if(stack.pop !== map[char]){
                 return false
             }
-            //matched , pop the top 
-            stack.pop()
         }
     }
-    //valid if no unmatched brackets remain 
     return stack.length === 0;
 }
