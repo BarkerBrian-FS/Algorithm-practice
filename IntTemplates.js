4d7ac5f80138f162558d01f721444dbdb4e8e985
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-25 10:54:44 -0600

    added tree bfs level order template

--- IntTemplates.js
+++ IntTemplates.js
@@ -138,3 +138,25 @@ function monotonicStack(nums){
         stack.push(i)
     }
 }
+
+//Tree Bfs level order
+function levelOrder(root){
+    const result = [];
+    const queue = [root];
+
+    while(queue.length){
+        const size = queue.length;
+        const level = [];
+
+        for(let i = 0; i < size; i++){
+            const node = queue.shift();
+
+            level.push(node.val);
+
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        result.push(level);
+    }
+    return result;
+}
