ecc0f9f2714d19fb98a8c0a03bcc05840117da81
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-25 10:25:52 -0600

    added hashmap algo template

---
+++ IntTemplates.js
@@ -0,0 +1,12 @@
+//Hashmap Template
+function example(nums){
+    const map = new Map();
+
+    for(let i =0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            return true
+        }
+        map.set(nums[i] , i)
+    }
+    return false
+}
