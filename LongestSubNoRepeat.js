74e73d80340fd8aed7f9d08b640c98a26203d965
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-26 12:05:41 -0600

    added longest substring no repears

--- LongestSubNoRepeat.js
+++ LongestSubNoRepeat.js
@@ -1,21 +1,22 @@
 function longestSubstringWithoutRepeatingCharacters(s) {
-    //set longest to 0
-    let longest = 0;
-    //make a set for the window
-    const window = new Set();
-    //start l pointer at beginning 
-    let l = 0;
-    //iterate through with right pointer
-    for(let right = 0; right<s.length; right++){
-        //check window for right index
-        while(widow.has(s[right])){
-            //if it has it delete left index and move left
-            widow.delete(s[l])
-            l++
+    //use set as window 
+    let set = new Set();
+    // make left pointer
+    let left = 0;
+    let maxLength = 0;
+    //iterate through s 
+    for(let right =0; right < s.length; right++){
+        //check if left same as right
+        while(set.has(s[right])){
+            //if same delete left
+            set.delete(s[left])
+            //move left forward
+            left++
         }
-        //if not in set add it and make new longest value
-        window.add(s[right]);
-        longest = Math.max(longest, right - l +1);
+        //add right into set 
+        set.add(s[right])
+        //compare length and change if necessary
+        maxLength = Math.max(maxLength, right - left +1)
     }
-    return longest;
+    return maxLength
 }
