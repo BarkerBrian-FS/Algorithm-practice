7f0157adbc9f7331571e2eb0715a7d426c13bc57
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-02-26 10:41:12 -0600

    added concise version of valid parethesis

--- validParethesis.js
+++ validParethesis.js
@@ -17,3 +17,21 @@ function isValid(s){
     }
     return stack.length === 0;
 }
+
+//Concise Version 
+
+function isValid(s){
+    let stack = [];
+    let map ={')':'(', '}':'{', ']':'['};
+
+    for(let char of s){
+        if(map[char]){
+            // char is a closing bracket
+            if(stack.pop() !== map[char]) return false;
+        } else {
+            //char is an opening bracket
+            stack.push(char)
+        }
+    }
+    return stack.length === 0;
+}
